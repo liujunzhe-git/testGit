edf26007474787fe19b70ecbbdc04fd808597e7d
ljz <ljz@sina.com>
2020-04-12 12:50:49 +0800

    master add!!!

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @Author JunZhe Liu
+ * @Date 2020/4/11 15:31
+ * @Version 2.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("master...add!!!");
+    }
+}
